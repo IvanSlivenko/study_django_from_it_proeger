e0f19cddd14e39911273449e8c2fb520ca96214c
IvanSlivenko <ivan8822@ukr.net>
2024-06-26 09:09:07 +0300

    26-06-2024-09-09

--- web-app/umanproger/news/urls.py
+++ web-app/umanproger/news/urls.py
@@ -9,6 +9,8 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailViews.as_view(), name='news-detail' ),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='news-update' ),
     
     
 ]
--- web-app/umanproger/news/views.py
+++ web-app/umanproger/news/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Articles
 from . forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView
+ 
 
 def news_home(request):
     news = Articles.objects.all().order_by('-date')
     return render(request, 'news/news_home.html', {'news': news })
+
+class NewsDetailViews(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+
    
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма була не вірною'
+
+
     
     form = ArticlesForm
 
     data ={
         'form': form,
+        'error' : error,
     }
     return render(request,'news/create.html', data)
